f8984b5147ce36f8972531e536d34c9f8baed687
CarolinaRMarques <44268230+CarolinaRMarques@users.noreply.github.com>
2021-06-24 11:08:17 +0100

    search box included

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/searchBox/searchBox.component";
 
 class App extends Component {
 	constructor() {
@@ -8,6 +9,7 @@ class App extends Component {
 
 		this.state = {
 			monsters: [],
+			searchField: "",
 		};
 	}
 
@@ -18,9 +20,17 @@ class App extends Component {
 	}
 
 	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
 		return (
 			<div className="App">
-				<CardList monsters={this.state.monsters} />
+				<SearchBox
+					placeholder="search monsters"
+					handleChange={(e) => this.setState({ searchField: e.target.value })}
+				/>
+				<CardList monsters={filteredMonsters} />
 			</div>
 		);
 	}
